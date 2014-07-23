6464ba7d3fea0ec99eb90c0d2ab6e8cfb83b17c6
Jose Pedro <1jpsneto@gmail.com>
2014-07-23 15:13:15 -0300

    Beginning the pbm's test

--- frequency_meter.m
+++ frequency_meter.m
@@ -1,33 +1,32 @@
-clear all
-%clc
-% opening the file
-file = open('metronomo/120bpm.wav');
-%file = open('metronomo/60bpm.wav');
-%file = open('metronomo/240bpm.wav');
-%file = open('oh_darling.wav');
-%file = open('smoke_on_the_water.wav');
-%file = open('i_can_see.wav');
-file.data = file.data(1 : file.fs*60);
-%file.data = file.data(file.fs*60*4 : file.fs*60*5);
-bpm_music = abs(file.data);
-%figure
-%1
-%plot(bpm_music)
+function number_of_peaks = frequency_meter(file_path)
+    % opening the file
+    %file = open('metronomo/120bpm.wav');
+    %file = open('metronomo/60bpm.wav');
+    %file = open('metronomo/240bpm.wav');
+    %file = open('oh_darling.wav');
+    %file = open('smoke_on_the_water.wav');
+    %file = open('i_can_see.wav');
+    file.data = file.data(1 : file.fs*60);
+    %file.data = file.data(file.fs*60*4 : file.fs*60*5);
+    bpm_music = abs(file.data);
+    %figure
+    %1
+    %plot(bpm_music)
 
-%filtering the pulses of minor energy
-signal_filtered = filter_signal(bpm_music);
+    %filtering the pulses of minor energy
+    signal_filtered = filter_signal(bpm_music);
 
-% Building array with means movies
-signal_pulses = decrease_resolution(signal_filtered, file.fs, 5);
+    % Building array with means movies
+    signal_pulses = decrease_resolution(signal_filtered, file.fs, 5);
 
-% Beginnnig the correlation
-array_correlation = correlate_moments(signal_pulses);
+    % Beginnnig the correlation
+    array_correlation = correlate_moments(signal_pulses);
 
-% Graph with correlation done! Now I will calculate the number of peaks
-array_peaks = filter_peak(signal_pulses);
+    % Graph with correlation done! Now I will calculate the number of peaks
+    array_peaks = filter_peak(signal_pulses);
 
-peaks = findpeaks(array_peaks);
+    peaks = findpeaks(array_peaks);
 
-number_of_peaks = length(peaks) + 1 %nao sei porque tenho que acrescentar mais 1
+    number_of_peaks = length(peaks) + 1; %nao sei porque tenho que acrescentar mais 1
 
-%analizar cada minuto da musica e usar o espaçamento de ponto a ponto para contar os bpm
+    %analizar cada minuto da musica e usar o espaçamento de ponto a ponto para contar os bpm
